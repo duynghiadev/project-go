7b85e81f42ce073daba89775df88206fecc167e0
duynghiadev <duynghia22302@gmail.com>
2025-01-17 16:19:31 +0700

    fix CORS golang

--- golang-todoapp-next/backend/go.mod
+++ golang-todoapp-next/backend/go.mod
@@ -2,8 +2,9 @@ module todo
 
 go 1.23.4
 
-require (
-	github.com/go-chi/chi/v5 v5.2.0
-)
+require github.com/go-chi/chi/v5 v5.2.0
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/go-chi/cors v1.2.1
+	github.com/lib/pq v1.10.9
+)
--- golang-todoapp-next/backend/main.go
+++ golang-todoapp-next/backend/main.go
@@ -8,6 +8,7 @@ import (
 	"todo/internal/database"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	_ "github.com/lib/pq"
 )
 
@@ -32,6 +33,17 @@ func main() {
 	}
 
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"http://localhost:3000"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	r.Post("/todos", apiConfig.handlePostTodos)
 	r.Delete("/todos/{id}", apiConfig.handleDeleteTodos)
