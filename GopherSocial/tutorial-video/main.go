9114392a380a5632a574caf552017f5833d50887
duynghiadev <duynghia22302@gmail.com>
2025-02-10 15:56:11 +0700

    HTTP Server - The net/http package

--- GopherSocial/tutorial-video/main.go
+++ GopherSocial/tutorial-video/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
+
+type api struct {
+	addr string
+}
+
+func (s *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		switch r.URL.Path {
+		case "/":
+			w.Write([]byte("GET method"))
+		case "/index":
+			w.Write([]byte("GET index"))
+		}
+	case http.MethodPost:
+		w.Write([]byte("POST method"))
+	}
+}
 
 func main() {
-	fmt.Println("hello world")
+	// implements the httpHandler interface
+	api := &api{addr: ":8080"}
+
+	srv := &http.Server{
+		Addr:    api.addr,
+		Handler: api,
+	}
+
+	srv.ListenAndServe()
 }
